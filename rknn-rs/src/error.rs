be25d22c718e882485db4b91dc5d17ab73630171
kautism <darkautism@gmail.com>
2025-12-14 16:35:39 +0800

    Make this lib more rustful style.

---
+++ rknn-rs/src/error.rs
@@ -0,0 +1,30 @@
+/// Error type for RKNN operations.
+#[derive(Debug)]
+pub struct Error(pub String);
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
+}
+
+#[macro_export]
+macro_rules! rkerr {
+    ($msg:expr, $code:expr) => {
+        Err($crate::error::Error(format!("{} exit code:{}", $msg, $code)))
+    };
+}
